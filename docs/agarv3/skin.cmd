bb222f2baa73dc6fe6097c26b1894931ba528108
Askarych <r.askarych@gmail.com>
2026-04-24 18:58:30 +0200

    Trim non-v1 docs variants

--- docs/agarv3/skin.cmd
+++
@@ -1,8 +0,0 @@
-REM --- Count the total number of PNG files in the skins folder ---
-set "total=0"
-for %%a in (skins\*.png) do (
-    set /a total+=1
-)
-echo Total skins found: %total%
-
-for %%a in (skins\*.png) do @echo|set /p="%%~na,">>skinList.txt
